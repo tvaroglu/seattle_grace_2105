ce85a51d2af64e1352acdf3263fc3e8f849995a6
Taylor Varoglu <tava8398@colorado.edu>
2021-05-26 11:02:21 -0600

    Add failed challenge :(

---
+++ spec/doctor_spec.rb
@@ -0,0 +1,33 @@
+require 'rspec'
+require_relative '../lib/doctor'
+
+RSpec.describe Doctor do
+  context 'it initializes' do
+    it 'with supplied args' do
+      doc = Doctor.new(
+        {
+          name: 'Meredith Grey',
+          specialty: 'General Surgery',
+          education: 'Harvard University',
+          salary: 100_000
+          }
+        )
+
+        expect(doc.class).to eq(Doctor)
+        expect(doc.name).to eq('Meredith Grey')
+        expect(doc.specialty).to eq('General Surgery')
+        expect(doc.education).to eq('Harvard University')
+        expect(doc.salary).to eq(100_000)
+      end
+      it 'with default params' do
+        default_doc = Doctor.new
+
+        expect(default_doc.class).to eq(Doctor)
+        expect(default_doc.name).to eq('Dr. House')
+        expect(default_doc.specialty).to eq('General')
+        expect(default_doc.education).to eq('Princeton')
+        expect(default_doc.salary).to eq(999999)
+      end
+  end
+
+end
---
+++ spec/hospital_spec.rb
@@ -0,0 +1,80 @@
+require 'rspec'
+require_relative '../lib/doctor'
+require_relative '../lib/hospital'
+
+
+RSpec.describe Hospital do
+  before :each do
+    @name = 'Seattle Grace'
+    @chief_of_surgery = 'Richard Webber'
+    @doctors = [
+      Doctor.new(
+        {
+          name: 'Meredith Grey',
+          specialty: 'General Surgery',
+          education: 'Harvard University',
+          salary: 100_000
+          }
+        ),
+      Doctor.new(
+        {
+          name: 'Alex Karev',
+          specialty: 'Pediatric Surgery',
+          education: 'Johns Hopkins University',
+          salary: 90_000
+          }
+        )
+      ]
+  end
+
+  context 'it initializes' do
+    it 'with supplied args' do
+      hospital = Hospital.new(@name, @chief_of_surgery, @doctors)
+
+      expect(hospital.class).to eq(Hospital)
+      expect(hospital.name).to eq('Seattle Grace')
+      expect(hospital.chief_of_surgery).to eq('Richard Webber')
+
+      expect(hospital.doctors.class).to eq(Array)
+      expect(hospital.doctors.length).to eq(2)
+
+      expect(hospital.doctors.first.name).to eq('Meredith Grey')
+      expect(hospital.doctors.first.specialty).to eq('General Surgery')
+      expect(hospital.doctors.first.education).to eq('Harvard University')
+      expect(hospital.doctors.first.salary).to eq(100_000)
+      expect(hospital.doctors.last.name).to eq('Alex Karev')
+      expect(hospital.doctors.last.specialty).to eq('Pediatric Surgery')
+      expect(hospital.doctors.last.education).to eq('Johns Hopkins University')
+      expect(hospital.doctors.last.salary).to eq(90_000)
+    end
+
+    it 'with default params' do
+      empty_hospital = Hospital.new
+
+      expect(empty_hospital.class).to eq(Hospital)
+      expect(empty_hospital.name).to eq('Mercy Hospital')
+      expect(empty_hospital.chief_of_surgery).to eq('Open Position')
+
+      expect(empty_hospital.doctors.class).to eq(Array)
+      expect(empty_hospital.doctors.length).to eq(0)
+
+
+      expect(empty_hospital.total_salary).to eq(0)
+      expect(empty_hospital.lowest_paid_doctor).to eq(nil)
+      expect(empty_hospital.specialties).to eq([])
+    end
+  end
+
+  it 'can return statistics' do
+    hospital = Hospital.new(@name, @chief_of_surgery, @doctors)
+
+    expect(hospital.total_salary).to eq(190000)
+    expect(hospital.lowest_paid_doctor).to eq('Alex Karev')
+
+    expect(hospital.specialties.class).to eq(Array)
+    expect(hospital.specialties.length).to eq(2)
+    expect(hospital.specialties.first).to eq('General Surgery')
+    expect(hospital.specialties.last).to eq('Pediatric Surgery')
+  end
+
+end
